6fb42acf257e0398bd7aeea8ad80550bc9ec9f0a
Thanathat Lumsan <thanathatlumsan@gmail.com>
2026-06-03 17:19:57 +0700

    feat: add variable initializations and calculations in exercises

--- ex-5.js
+++ ex-5.js
@@ -1,3 +1,6 @@
 //Exercise #5: Shopping and Discount
 
-// Start coding here
+let productPrice = 27000;
+let discount = (productPrice * 25) / 100;
+let totalPrice = productPrice - discount;
+console.log(totalPrice)
